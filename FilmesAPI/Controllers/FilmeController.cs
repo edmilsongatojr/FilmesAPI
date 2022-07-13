5d26807d94e661e1cfea30a3d2fc220bbde7032d
Edmilson Gato Junior <edygatojr@live.com>
2022-07-13 13:21:13 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FilmesAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody] Filme filme)
+        {
+
+            filmes.Add(filme);
+            System.Console.WriteLine(filme.Titulo);
+        }
+    }
+}
