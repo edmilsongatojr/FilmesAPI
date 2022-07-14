28bb3e98abdaccfe85350f7147dbc422c9b9f5a6
Edmilson Gato Junior <edygatojr@live.com>
2022-07-13 22:16:40 -0300

    Definindo o retorno das rotas. Utilizado o IActionResult nos métodos e refatorado o código.

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -16,23 +16,29 @@ namespace FilmesAPI.Controllers
         private static int id = 1;
 
         [HttpPost]
-        public void AdicionarFilme([FromBody] Filme filme)
+        public IActionResult AdicionarFilme([FromBody] Filme filme)
         {
             filme.Id = id++;
             filmes.Add(filme);
             //Console.WriteLine(filme.Titulo);
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { id = filme.Id }, filme);
         }
 
         [HttpGet]
-        public IEnumerable<Filme> RecuperaFilmes()
+        public IActionResult RecuperaFilmes()
         {
-            return filmes;
+            return Ok(filmes);
         }
 
         [HttpGet("{id}")]
-        public Filme RecuperaFilmesPorId(int id)
+        public IActionResult RecuperaFilmesPorId(int id)
         {
-            return filmes.FirstOrDefault(filme => filme.Id == id);
+           Filme  filme= filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme != null) 
+            {
+                return Ok(filme);
+            }
+            return NotFound();
         }
     }
 }
