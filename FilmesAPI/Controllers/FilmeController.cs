eeda3ba97a5f83825b1a3115307572d36c49562e
Edmilson Gato Junior <edygatojr@live.com>
2022-07-13 21:45:18 -0300

    Retornando informações da API - GET Criado a propriedade ID para termos um identificado único e o método IEnumerable<Filme> RecuperarFilmes.

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -12,13 +12,20 @@ namespace FilmesAPI.Controllers
 
     {
         private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
 
         [HttpPost]
         public void AdicionarFilme([FromBody] Filme filme)
         {
-
+            filme.Id = id++;
             filmes.Add(filme);
             //Console.WriteLine(filme.Titulo);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperarFilmes()
+        {
+            return filmes;
         }
     }
 }
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,6 +4,7 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        public int Id { get; internal set; }
         [Required(ErrorMessage ="O Campo Titulo é Obrigatório")]
         public string Titulo { get; set; }
         [Required(ErrorMessage = "O Campo Titulo é Obrigatório")]
@@ -11,5 +12,6 @@ namespace FilmesAPI.Models
         public string Genero { get; set; }
         [Range(1,600,ErrorMessage ="Tamanho invalido: A durãção precisa ter no mninimo 1m e no máximo 600m")]
         public int Duracao { get; set; }
+        
     }
 }
