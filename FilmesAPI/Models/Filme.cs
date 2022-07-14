d33da89147895a56e3ea0628eb0cb474a03b7780
Edmilson Gato Junior <edygatojr@live.com>
2022-07-13 22:43:58 -0300

    Gerando a primeira migration. Add-Migration CriandoTabeladeFilmes Update-Migration

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,10 +4,12 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Key]
+        [Required(ErrorMessage = "O ID é Obrigatório")]
         public int Id { get; internal set; }
         [Required(ErrorMessage ="O Campo Titulo é Obrigatório")]
         public string Titulo { get; set; }
-        [Required(ErrorMessage = "O Campo Titulo é Obrigatório")]
+        [Required(ErrorMessage = "O Campo Diretor é Obrigatório")]
         public string Diretor { get; set; }
         public string Genero { get; set; }
         [Range(1,600,ErrorMessage ="Tamanho invalido: A durãção precisa ter no mninimo 1m e no máximo 600m")]
