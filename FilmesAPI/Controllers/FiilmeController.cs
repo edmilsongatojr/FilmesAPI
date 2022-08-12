5aef58aa0037f423bb3746ea29f417733fc65fa0
Edmilson Gato Junior <edygatojr@live.com>
2022-08-11 23:28:44 -0300

    Implentação das Dtos e ajustes das classes.

--- FilmesAPI/Controllers/FiilmeController.cs
+++ FilmesAPI/Controllers/FiilmeController.cs
@@ -1,6 +1,8 @@
 ﻿using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
 using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,7 +12,7 @@ namespace FilmesAPI.Controllers
     [Route("[controller]")]
     public class FiilmeController : ControllerBase
     {
-      
+
         private FilmeContext _context;
 
         public FiilmeController(FilmeContext context)
@@ -18,27 +20,74 @@ namespace FilmesAPI.Controllers
             _context = context;
         }
         [HttpPost]
-        public IActionResult AdicionarFilme([FromBody] Filme filme)
+        public IActionResult AdicionarFilme([FromBody] CreateFilmeDto filmeDto)
         {
-            
+            Filme filme = new Filme
+            {
+                Titulo = filmeDto.Titulo,
+                Genero = filmeDto.Genero,
+                Duracao = filmeDto.Duracao,
+                Diretor = filmeDto.Diretor,
+            };        
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(RecuperarFilmesPorId), new { Id = filme.Id }, filme);
+        
         }
 
         [HttpGet]
-        public IActionResult RetornarFilmes()
+        public IEnumerable<Filme> RetornarFilmes()
         {
-            return Ok();
+            return _context.Filmes;
         }
 
         [HttpGet("{id}")]
         public IActionResult RecuperarFilmesPorId(int id)
         {
-            Filme filme = .FirstOrDefault(filme => filme.Id == id);
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
             if (filme != null)
             {
+                ReadFilmeDto filmeDto = new ReadFilmeDto
+                {
+                    Id = filme.Id,
+                    Titulo = filme.Titulo,
+                    Diretor = filme.Diretor,
+                    Duracao = filme.Duracao,
+                    Genero = filme.Genero,
+                    HoraDaConsulta = DateTime.Now
+                };
                 return Ok(filme);
             }
             return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizarFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            filme.Titulo = filmeDto.Titulo;
+            filme.Genero = filmeDto.Genero;
+            filme.Diretor = filmeDto.Diretor;
+            filme.Duracao = filmeDto.Duracao;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletarFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
